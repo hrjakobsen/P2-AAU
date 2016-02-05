dda134c882088da274a978eb12429f59dd651356
Henrik Herbst Sørensen <Henrik Herbst Srensen>
2016-02-05 21:11:09 +0100

    Made simple UI. Still working on cleaning code

--- Hund_I_kat/Hund_I_kat/Form1.Designer.cs
+++ Hund_I_kat/Hund_I_kat/Form1.Designer.cs
@@ -23,36 +23,140 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.getFileVessel = new System.Windows.Forms.OpenFileDialog();
+            this.getFilePlain = new System.Windows.Forms.OpenFileDialog();
+            this.picVessel = new System.Windows.Forms.PictureBox();
+            this.picPlain = new System.Windows.Forms.PictureBox();
+            this.loadVessel = new System.Windows.Forms.Button();
+            this.loadPlain = new System.Windows.Forms.Button();
+            this.encrypt = new System.Windows.Forms.Button();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.picCrypto = new System.Windows.Forms.PictureBox();
+            this.loadCrypto = new System.Windows.Forms.Button();
+            this.getFileCrypto = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picVessel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCrypto)).BeginInit();
             this.SuspendLayout();
             // 
-            // openFileDialog1
+            // getFileVessel
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            this.getFileVessel.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
-            // openFileDialog2
+            // getFilePlain
             // 
-            this.openFileDialog2.FileName = "openFileDialog1";
-            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            this.getFilePlain.FileName = "Select an image with half the width and height of the vessel image";
+            this.getFilePlain.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // picVessel
+            // 
+            this.picVessel.Location = new System.Drawing.Point(12, 12);
+            this.picVessel.Name = "picVessel";
+            this.picVessel.Size = new System.Drawing.Size(216, 216);
+            this.picVessel.TabIndex = 0;
+            this.picVessel.TabStop = false;
+            // 
+            // picPlain
+            // 
+            this.picPlain.Location = new System.Drawing.Point(234, 33);
+            this.picPlain.Name = "picPlain";
+            this.picPlain.Size = new System.Drawing.Size(108, 108);
+            this.picPlain.TabIndex = 1;
+            this.picPlain.TabStop = false;
+            // 
+            // loadVessel
+            // 
+            this.loadVessel.Location = new System.Drawing.Point(77, 234);
+            this.loadVessel.Name = "loadVessel";
+            this.loadVessel.Size = new System.Drawing.Size(86, 23);
+            this.loadVessel.TabIndex = 2;
+            this.loadVessel.Text = "Load image";
+            this.loadVessel.UseVisualStyleBackColor = true;
+            this.loadVessel.Click += new System.EventHandler(this.loadVessel_Click);
+            // 
+            // loadPlain
+            // 
+            this.loadPlain.Location = new System.Drawing.Point(245, 147);
+            this.loadPlain.Name = "loadPlain";
+            this.loadPlain.Size = new System.Drawing.Size(86, 23);
+            this.loadPlain.TabIndex = 3;
+            this.loadPlain.Text = "Load image";
+            this.loadPlain.UseVisualStyleBackColor = true;
+            this.loadPlain.Click += new System.EventHandler(this.loadPlain_Click);
+            // 
+            // encrypt
+            // 
+            this.encrypt.Location = new System.Drawing.Point(245, 176);
+            this.encrypt.Name = "encrypt";
+            this.encrypt.Size = new System.Drawing.Size(86, 23);
+            this.encrypt.TabIndex = 4;
+            this.encrypt.Text = "Encrypt";
+            this.encrypt.UseVisualStyleBackColor = true;
+            this.encrypt.Click += new System.EventHandler(this.encrypt_Click);
+            // 
+            // decrypt
+            // 
+            this.decrypt.Location = new System.Drawing.Point(245, 205);
+            this.decrypt.Name = "decrypt";
+            this.decrypt.Size = new System.Drawing.Size(86, 23);
+            this.decrypt.TabIndex = 5;
+            this.decrypt.Text = "Decrypt";
+            this.decrypt.UseVisualStyleBackColor = true;
+            this.decrypt.Click += new System.EventHandler(this.decrypt_Click);
+            // 
+            // picCrypto
+            // 
+            this.picCrypto.Location = new System.Drawing.Point(348, 12);
+            this.picCrypto.Name = "picCrypto";
+            this.picCrypto.Size = new System.Drawing.Size(216, 216);
+            this.picCrypto.TabIndex = 6;
+            this.picCrypto.TabStop = false;
+            // 
+            // loadCrypto
+            // 
+            this.loadCrypto.Location = new System.Drawing.Point(414, 234);
+            this.loadCrypto.Name = "loadCrypto";
+            this.loadCrypto.Size = new System.Drawing.Size(86, 23);
+            this.loadCrypto.TabIndex = 7;
+            this.loadCrypto.Text = "Load image";
+            this.loadCrypto.UseVisualStyleBackColor = true;
+            this.loadCrypto.Click += new System.EventHandler(this.loadCrypto_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 370);
+            this.ClientSize = new System.Drawing.Size(576, 262);
+            this.Controls.Add(this.loadCrypto);
+            this.Controls.Add(this.picCrypto);
+            this.Controls.Add(this.decrypt);
+            this.Controls.Add(this.encrypt);
+            this.Controls.Add(this.loadPlain);
+            this.Controls.Add(this.loadVessel);
+            this.Controls.Add(this.picPlain);
+            this.Controls.Add(this.picVessel);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picVessel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCrypto)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.OpenFileDialog getFileVessel;
+        private System.Windows.Forms.OpenFileDialog getFilePlain;
+        private System.Windows.Forms.PictureBox picVessel;
+        private System.Windows.Forms.PictureBox picPlain;
+        private System.Windows.Forms.Button loadVessel;
+        private System.Windows.Forms.Button loadPlain;
+        private System.Windows.Forms.Button encrypt;
+        private System.Windows.Forms.Button decrypt;
+        private System.Windows.Forms.PictureBox picCrypto;
+        private System.Windows.Forms.Button loadCrypto;
+        private System.Windows.Forms.OpenFileDialog getFileCrypto;
     }
 }
 
--- Hund_I_kat/Hund_I_kat/Form1.cs
+++ Hund_I_kat/Hund_I_kat/Form1.cs
@@ -10,131 +10,140 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1 {
     public partial class Form1:Form {
-        public Bitmap originalImg;
-        public Bitmap secretImg;
+        public Bitmap vesselImg;
+        public Bitmap plainImg;
 
         public Form1() {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
-            openFileDialog1.ShowDialog();
+        private void loadVessel_Click(object sender, EventArgs e) {
+            getFileVessel.ShowDialog();
         }
 
+        private void loadPlain_Click(object sender, EventArgs e) {
+            getFilePlain.ShowDialog();
+        }
+
+        private void loadCrypto_Click(object sender, EventArgs e) {
+            getFileCrypto.ShowDialog();
+        }
+
+        private void encrypt_Click(object sender, EventArgs e) {
+
+        }
+
+        private void decrypt_Click(object sender, EventArgs e) {
+
+        }
+
+
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e) {
-            originalImg = new Bitmap(openFileDialog1.FileName);
-
-            decodeImg();
-
-            //openFileDialog2.ShowDialog();
-            Application.Exit();
+            vesselImg = new Bitmap(getFileVessel.FileName);
         }
 
         private void openFileDialog2_FileOk(object sender, CancelEventArgs e) {
-            secretImg = new Bitmap(openFileDialog2.FileName);
-
-            combineImgs();
+            plainImg = new Bitmap(getFilePlain.FileName);
         }
 
         public void combineImgs() {
-            Bitmap finalImg = new Bitmap(originalImg.Width, originalImg.Height);
+            Bitmap cryptoImg = new Bitmap(vesselImg.Width, vesselImg.Height);
 
-            for (int y = 0; y < originalImg.Height; y++) {
-                for (int x = 0; x < originalImg.Width; x++) {
-                    finalImg.SetPixel(x, y, copyOriginal(originalImg.GetPixel(x, y)));
+            for (int y = 0; y < vesselImg.Height; y++) {
+                for (int x = 0; x < vesselImg.Width; x++) {
+                    cryptoImg.SetPixel(x, y, copyvessel(vesselImg.GetPixel(x, y)));
                 }
             }
 
-            Color[] secretPixels = new Color[secretImg.Height * secretImg.Width];
+            Color[] plainPixels = new Color[plainImg.Height * plainImg.Width];
 
-            for (int y = 0; y < secretImg.Height; y++) {
-                for (int x = 0; x < secretImg.Width; x++) {
-                    secretPixels[y*secretImg.Width + x] = secretImg.GetPixel(x, y);
+            for (int y = 0; y < plainImg.Height; y++) {
+                for (int x = 0; x < plainImg.Width; x++) {
+                    plainPixels[y*plainImg.Width + x] = plainImg.GetPixel(x, y);
                 }
             }
 
-            int secretPixelsCounter = 0;
+            int plainPixelsCounter = 0;
 
-            for (int y = 0; y < originalImg.Height; y++) {
-                for (int x = 0; x < originalImg.Width; x += 4) {
-                    for (int secretByte = 0; secretByte < 4; secretByte++) {
-                        finalImg.SetPixel(x + secretByte, y, addCol(finalImg.GetPixel(x + secretByte, y), secretPixels[secretPixelsCounter], secretByte));
+            for (int y = 0; y < vesselImg.Height; y++) {
+                for (int x = 0; x < vesselImg.Width; x += 4) {
+                    for (int plainByte = 0; plainByte < 4; plainByte++) {
+                        cryptoImg.SetPixel(x + plainByte, y, addCol(cryptoImg.GetPixel(x + plainByte, y), plainPixels[plainPixelsCounter], plainByte));
                     }
-                    secretPixelsCounter++;
+                    plainPixelsCounter++;
                 }
             }
 
-            finalImg.Save("C:\\Users\\hhs19\\Downloads\\final.bmp");
+            cryptoImg.Save("C:\\Users\\hhs19\\Downloads\\crypto.bmp");
         }
 
         public void decodeImg() {
-            Bitmap finalImg = new Bitmap(originalImg.Width / 2, originalImg.Height / 2);
-            Color[] originalPixel = new Color[originalImg.Height * originalImg.Width];
-            int originalPixelsCounter = 0;
+            Bitmap cryptoImg = new Bitmap(vesselImg.Width / 2, vesselImg.Height / 2);
+            Color[] vesselPixel = new Color[vesselImg.Height * vesselImg.Width];
+            int vesselPixelsCounter = 0;
 
-            for (int y = 0; y < originalImg.Height; y++) {
-                for (int x = 0; x < originalImg.Width; x++) {
-                    originalPixel[y * originalImg.Width + x] = originalImg.GetPixel(x, y);
+            for (int y = 0; y < vesselImg.Height; y++) {
+                for (int x = 0; x < vesselImg.Width; x++) {
+                    vesselPixel[y * vesselImg.Width + x] = vesselImg.GetPixel(x, y);
                 }
             }
 
-            for (int y = 0; y < finalImg.Height; y++) {
-                for (int x = 0; x < finalImg.Width; x++) {
-                    Color combinedColor = combineToColor(originalPixel[originalPixelsCounter], originalPixel[originalPixelsCounter + 1], originalPixel[originalPixelsCounter + 2], originalPixel[originalPixelsCounter + 3]);
-                    finalImg.SetPixel(x, y, combinedColor);
-                    originalPixelsCounter += 4;
+            for (int y = 0; y < cryptoImg.Height; y++) {
+                for (int x = 0; x < cryptoImg.Width; x++) {
+                    Color combinedColor = combineToColor(vesselPixel[vesselPixelsCounter], vesselPixel[vesselPixelsCounter + 1], vesselPixel[vesselPixelsCounter + 2], vesselPixel[vesselPixelsCounter + 3]);
+                    cryptoImg.SetPixel(x, y, combinedColor);
+                    vesselPixelsCounter += 4;
                 }
             }
 
-            finalImg.Save("C:\\Users\\hhs19\\Downloads\\decoded.bmp");
+            cryptoImg.Save("C:\\Users\\hhs19\\Downloads\\decoded.bmp");
             Application.Exit();
         }
 
-        public Color copyOriginal(Color originalCol) {
-            byte maskOriginal = 0xFC;
-            Color finalCol = new Color();
+        public Color copyvessel(Color vesselCol) {
+            byte maskvessel = 0xFC;
+            Color cryptoCol = new Color();
             byte R, G, B;
 
-            R = (byte)(originalCol.R & maskOriginal);
-            G = (byte)(originalCol.G & maskOriginal);
-            B = (byte)(originalCol.B & maskOriginal);
+            R = (byte)(vesselCol.R & maskvessel);
+            G = (byte)(vesselCol.G & maskvessel);
+            B = (byte)(vesselCol.B & maskvessel);
 
-            finalCol = Color.FromArgb(R, G, B);
+            cryptoCol = Color.FromArgb(R, G, B);
 
-            return finalCol;
+            return cryptoCol;
         }
 
-        public Color addCol(Color originalCol, Color secretCol, int secretByte) {
-            byte[] maskSecret = new byte[] { 0x3, 0xC, 0x30, 0xC0 };
-            Color finalCol = new Color();
+        public Color addCol(Color vesselCol, Color plainCol, int plainByte) {
+            byte[] maskplain = new byte[] { 0x3, 0xC, 0x30, 0xC0 };
+            Color cryptoCol = new Color();
             byte R, G, B;
 
-            R = (byte)((byte)((secretCol.R & maskSecret[secretByte]) >> (secretByte * 2)) + originalCol.R);
-            G = (byte)((byte)((secretCol.G & maskSecret[secretByte]) >> (secretByte * 2)) + originalCol.G);
-            B = (byte)((byte)((secretCol.B & maskSecret[secretByte]) >> (secretByte * 2)) + originalCol.B);
+            R = (byte)((byte)((plainCol.R & maskplain[plainByte]) >> (plainByte * 2)) + vesselCol.R);
+            G = (byte)((byte)((plainCol.G & maskplain[plainByte]) >> (plainByte * 2)) + vesselCol.G);
+            B = (byte)((byte)((plainCol.B & maskplain[plainByte]) >> (plainByte * 2)) + vesselCol.B);
 
-            finalCol = Color.FromArgb(R, G, B);
+            cryptoCol = Color.FromArgb(R, G, B);
 
-            return finalCol;
+            return cryptoCol;
         }
 
         public Color combineToColor(Color c1, Color c2, Color c3, Color c4) {
-            byte maskSecret = 0x3;
-            Color finalCol = new Color();
+            byte maskplain = 0x3;
+            Color cryptoCol = new Color();
             byte R, G, B;
 
-            R = (byte)((byte)(c1.R & maskSecret) << 6);
-            R += (byte)((byte)(c2.R & maskSecret) << 4);
-            R += (byte)((byte)(c3.R & maskSecret) << 2);
-            R += (byte)(c4.R & maskSecret);
+            R = (byte)((byte)(c1.R & maskplain) << 6);
+            R += (byte)((byte)(c2.R & maskplain) << 4);
+            R += (byte)((byte)(c3.R & maskplain) << 2);
+            R += (byte)(c4.R & maskplain);
             
-            G = (byte)((byte)((byte)(c1.G & maskSecret) << 6) + (byte)((byte)(c2.G & maskSecret) << 4) + (byte)((byte)(c3.G & maskSecret) << 2) + ((byte)(c4.G & maskSecret)));
-            B = (byte)((byte)((byte)(c1.B & maskSecret) << 6) + (byte)((byte)(c2.B & maskSecret) << 4) + (byte)((byte)(c3.B & maskSecret) << 2) + ((byte)(c4.B & maskSecret)));
+            G = (byte)((byte)((byte)(c1.G & maskplain) << 6) + (byte)((byte)(c2.G & maskplain) << 4) + (byte)((byte)(c3.G & maskplain) << 2) + ((byte)(c4.G & maskplain)));
+            B = (byte)((byte)((byte)(c1.B & maskplain) << 6) + (byte)((byte)(c2.B & maskplain) << 4) + (byte)((byte)(c3.B & maskplain) << 2) + ((byte)(c4.B & maskplain)));
             
-            finalCol = Color.FromArgb(R, G, B);
+            cryptoCol = Color.FromArgb(R, G, B);
 
-            return finalCol;
+            return cryptoCol;
         }
-
     }
 }
