206c1aa810e7dd2c54dfbf17fbf28cf68a80efca
Mathias Jakobsen <Mathias Jakobsen>
2016-02-06 12:25:17 +0100

    Mathias's løsning på hund I kat problemet

---
+++ Programmer/ImageEncoder/ImageEncoder/ImageEncoding.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace ImageEncoding
+{
+    public class ImageEncoder
+    {
+        private Bitmap ImageToEncode;
+
+        public ImageEncoder(string FilePath) {
+            if (!string.IsNullOrEmpty(FilePath)) {
+                try {
+                    ImageToEncode = new Bitmap(FilePath);
+                } catch (FileNotFoundException e) {
+                    Console.WriteLine($"{FilePath} not found. Make sure it is a valid file path. {e.Message}");
+                    throw;
+                }
+            } else {
+                throw new System.ArgumentException("String cannot be null or empty");
+            }
+        }
+
+        public ImageEncoder(Bitmap ImageToBeEncoded) {
+            if (ImageToBeEncoded != null) {
+                ImageToEncode = ImageToBeEncoded;
+            } else {
+                throw new ArgumentException("Image cannot be null");
+            }
+        }
+
+        public Bitmap EncodeToImage(string FilePath) {
+            Bitmap ImageToEncodeIn;
+            if (!string.IsNullOrEmpty(FilePath)) {
+                try {
+                    ImageToEncodeIn = new Bitmap(FilePath);
+                } catch (FileNotFoundException e) {
+                    Console.WriteLine($"{FilePath} not found. Make sure it is a valid file path. {e.Message}");
+                    throw;
+                }
+            } else {
+                throw new System.ArgumentException("String cannot be null or empty");
+            }
+
+            return EncodeToImage(ImageToEncodeIn);
+        }
+
+        public Bitmap EncodeToImage(Bitmap ImageToEncodeIn) {
+            if (ImageToEncode.Width * 2 != ImageToEncodeIn.Width || ImageToEncode.Height * 2 != ImageToEncodeIn.Height) {
+                throw new ArgumentException("The width and height of the image to encode in must be double the size of the image to encode, in both dimensions");
+            }
+
+            Color[] ImageToEncodeArr = ImageTo1DArr(ImageToEncode);
+            Color[] ImageToEncodeInArr = ImageTo1DArr(ImageToEncodeIn);
+            Color[] ResultImageArr = new Color[ImageToEncodeIn.Width * ImageToEncodeIn.Height];
+
+
+            int EncoderCounter = 0;
+            byte OriginalMask = 0xFC /* Grabs 1111 1100 */
+            byte[] ExtractMasks = new byte[] { 0xC0, 0x30, 0xC, 0x3}; /* 1100 0000, 0011 0000, 0000 1100, 0000 0011 */
+            byte R, G, B;
+            
+            for (int j = 0; j < ImageToEncodeInArr.Length; j += 4) {
+                for (int i = 0; i < 4; i++) {
+                    R = (byte)((byte)(ImageToEncodeInArr[j + i].R & OriginalMask) + ((byte)(ImageToEncodeArr[EncoderCounter].R & ExtractMasks[i]) >> 2 * (3 - i)));
+                    G = (byte)((byte)(ImageToEncodeInArr[j + i].G & OriginalMask) + ((byte)(ImageToEncodeArr[EncoderCounter].G & ExtractMasks[i]) >> 2 * (3 - i)));
+                    B = (byte)((byte)(ImageToEncodeInArr[j + i].B & OriginalMask) + ((byte)(ImageToEncodeArr[EncoderCounter].B & ExtractMasks[i]) >> 2 * (3 - i)));
+                   
+                    ResultImageArr[j + i] = Color.FromArgb(R, G, B);
+                }
+                EncoderCounter++;
+            }
+
+            Bitmap ResultImage = ArrToImage(ImageToEncodeIn.Width, ImageToEncodeIn.Height, ResultImageArr);
+
+            return ResultImage;
+        }
+
+        private static Color[] ImageTo1DArr(Bitmap ImageToConvert) {
+            Color[] Arr = new Color[ImageToConvert.Width * ImageToConvert.Height];
+
+            for (int i = 0; i < ImageToConvert.Height; i++) {
+                for (int j = 0; j < ImageToConvert.Width; j++) {
+                    Arr[i * ImageToConvert.Height + j] = ImageToConvert.GetPixel(j, i);
+                }
+            }
+
+            return Arr;
+        }
+
+        private static Bitmap ArrToImage(int Width, int Height, Color[] Arr) {
+            Bitmap ResultImage = new Bitmap(Width, Height);
+
+            int ArrCounter = 0;
+            for (int i = 0; i < Height; i++) {
+                for (int j = 0; j < Width; j++) {
+                    ResultImage.SetPixel(j, i, Arr[ArrCounter]);
+                    ArrCounter++;
+                }
+            }
+
+            return ResultImage;
+        }
+
+        public static Bitmap ExtractImageFromImage(string FilePath) {
+            if (string.IsNullOrEmpty(FilePath)) {
+                throw new ArgumentException("String cannot be null or empty.");
+            }
+            Bitmap ImageToExtractFrom = new Bitmap(FilePath);
+
+            return ExtractImageFromImage(ImageToExtractFrom);
+        }
+
+        public static Bitmap ExtractImageFromImage(Bitmap OriginalImage) {
+            if (OriginalImage == null) {
+                throw new ArgumentNullException();
+            }
+            
+
+            Color[] OriginalImageArr = ImageTo1DArr(OriginalImage);
+            Color[] ResultImageArr = new Color[OriginalImage.Width / 2 * OriginalImage.Height / 2];
+
+            byte ExtractMask = 0x3;
+
+            for (int i = 0; i < ResultImageArr.Length; i++) {
+                byte R = 0, G = 0, B = 0;
+                for (int j = 0; j < 4; j++) {
+                    R += (byte)((byte)(OriginalImageArr[i * 4 + j].R & ExtractMask) << ((3 - j) * 2));
+                    G += (byte)((byte)(OriginalImageArr[i * 4 + j].G & ExtractMask) << ((3 - j) * 2));
+                    B += (byte)((byte)(OriginalImageArr[i * 4 + j].B & ExtractMask) << ((3 - j) * 2));
+                }
+                ResultImageArr[i] = Color.FromArgb(R, G, B);
+            }
+
+            Bitmap ResultImage = ArrToImage(OriginalImage.Width / 2, OriginalImage.Height / 2, ResultImageArr);
+
+            return ResultImage;
+        }
+
+    }
+}
---
+++ Programmer/ImageEncoder/TestOfImageEncoder/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TestOfImageEncoder {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.EncodedImagePicker = new System.Windows.Forms.OpenFileDialog();
+            this.EncodedToPicker = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.ExtractPicker = new System.Windows.Forms.OpenFileDialog();
+            this.SaveEncoded = new System.Windows.Forms.SaveFileDialog();
+            this.SaveExtracted = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose image to be encoded";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Choose image to be encoded in";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 71);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Start encoding";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // EncodedImagePicker
+            // 
+            this.EncodedImagePicker.FileOk += new System.ComponentModel.CancelEventHandler(this.EncodedImagePicker_FileOk);
+            // 
+            // EncodedToPicker
+            // 
+            this.EncodedToPicker.FileOk += new System.ComponentModel.CancelEventHandler(this.EncodedToPicker_FileOk);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 142);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(178, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Choose File to extract from";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 171);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(179, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Start Extracting";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // ExtractPicker
+            // 
+            this.ExtractPicker.FileOk += new System.ComponentModel.CancelEventHandler(this.ExtractPicker_FileOk);
+            // 
+            // SaveEncoded
+            // 
+            this.SaveEncoded.FileOk += new System.ComponentModel.CancelEventHandler(this.SaveEncoded_FileOk);
+            // 
+            // SaveExtracted
+            // 
+            this.SaveExtracted.FileOk += new System.ComponentModel.CancelEventHandler(this.SaveExtracted_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Encoder and decoder of images";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog EncodedImagePicker;
+        private System.Windows.Forms.OpenFileDialog EncodedToPicker;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog ExtractPicker;
+        private System.Windows.Forms.SaveFileDialog SaveEncoded;
+        private System.Windows.Forms.SaveFileDialog SaveExtracted;
+    }
+}
+
