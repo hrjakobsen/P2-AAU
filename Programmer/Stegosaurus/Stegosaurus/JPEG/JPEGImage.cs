32528c304ba78f2b7bb424a85b7d7e779a202487
hu <jacobsvenningsen15@hotmail.com>
2016-04-14 14:51:55 +0200

    påbegyndelse on le Decoder

--- Programmer/Stegosaurus/Stegosaurus/JPEG/JPEGImage.cs
+++ Programmer/Stegosaurus/Stegosaurus/JPEG/JPEGImage.cs
@@ -120,6 +120,17 @@ namespace Stegosaurus {
         }
 
         /// <summary>
+        /// Used to decode the hidden message in given file
+        /// </summary>
+        /// <param name="path"></param>
+        public void Decode(string path) {
+            StreamReader sr = new StreamReader(path);
+            BinaryReader file = new BinaryReader(sr.BaseStream);
+            byte[] byteArr = findScanData(file);
+            byteArr = GetMessage(byteArr);
+        }
+
+        /// <summary>
         /// Saves an encoded jpeg image to a filesystem
         /// </summary>
         /// <param name="Path"></param>
@@ -490,6 +501,74 @@ namespace Stegosaurus {
 
         public int GetCapacity() {
             throw new NotImplementedException();
+        }
+
+        private byte[] findScanData(BinaryReader file) {
+            byte a;
+            bool foundMarker = false;
+            while (!foundMarker) {
+                a = file.ReadByte();
+                if (a == 0xff) {
+                    a = file.ReadByte();
+                    if (a == 0xda) {
+                        foundMarker = true;
+                    }
+                }
+            }
+            foundMarker = false;
+            List<byte> listOfBytes = new List<byte>();
+            while (!foundMarker) {
+                a = file.ReadByte();
+
+                if (a == 0xff) {
+                    byte b = file.ReadByte();
+                    if (b != 0) {
+                        break;
+                    }
+                    listOfBytes.Add(a);
+                    listOfBytes.Add(b);
+                } else {
+                    listOfBytes.Add(a);
+                }
+            }
+            return listOfBytes.ToArray();
+        }
+
+        private byte[] GetMessage(byte[] scanData) {
+            // int m = scanData[0];
+            // m is the modulo operator saved somewhere in scanData
+            int m = 4;
+            List<byte> byteList = new List<byte>();
+            int i;
+            for (i = 1; i < scanData.Length; i++) {
+                byteList.Add((byte)((scanData[i] + scanData[i++]) % m));
+            }
+
+            int iterations = (int)(8/Math.Log(m,2));
+            int logM = (int)Math.Log(m, 2);
+            List<byte> message = new List<byte>();
+            i = 1;
+            while(i < byteList.Count) {
+                byte byteToAdd = 0;
+                for (int j = 0; j < iterations; j++) {
+                    if (i < byteList.Count) {
+                        // extracts the message from the bytelist and bitshifts the appropriate amount of times.
+                        // LogM describes how many bits we've got the information in
+                        // iterations - j + 1 times LogM ensures we've added the bits in the right spots
+                        byteToAdd += (byte)(byteList[i] << (logM * (iterations - j + 1)));
+                        i++;
+                    }
+                }
+                message.Add(byteToAdd);
+            }
+            List<byte> actualMessage = new List<byte>();
+
+            //byteList[4] is the position where the length of the message is saved
+            int read = byteList[4];
+            for (i = 0; i < read; i++) {
+                actualMessage.Add(message[i]);
+            }
+            return actualMessage.ToArray();
         }
     }
 }
