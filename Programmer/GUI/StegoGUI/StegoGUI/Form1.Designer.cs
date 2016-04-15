f997671d2a0761f94e6f3090233b9bdb7fb9cad1
Flax95 <95soren@gmail.com>
2016-04-15 10:13:48 +0200

    GUI tilføjet

---
+++ Programmer/GUI/StegoGUI/StegoGUI/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace StegoGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picStego = new System.Windows.Forms.PictureBox();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.loadMessage = new System.Windows.Forms.Button();
+            this.loadCover = new System.Windows.Forms.Button();
+            this.picMessage = new System.Windows.Forms.PictureBox();
+            this.picCover = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.showHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.picStego)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMessage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCover)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picStego
+            // 
+            this.picStego.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picStego.Location = new System.Drawing.Point(352, 30);
+            this.picStego.Name = "picStego";
+            this.picStego.Size = new System.Drawing.Size(216, 216);
+            this.picStego.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picStego.TabIndex = 13;
+            this.picStego.TabStop = false;
+            this.picStego.Click += new System.EventHandler(this.picStego_Click);
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(249, 223);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(86, 23);
+            this.btnDecode.TabIndex = 12;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Enabled = false;
+            this.btnEncode.Location = new System.Drawing.Point(249, 194);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(86, 23);
+            this.btnEncode.TabIndex = 11;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // loadMessage
+            // 
+            this.loadMessage.Location = new System.Drawing.Point(249, 165);
+            this.loadMessage.Name = "loadMessage";
+            this.loadMessage.Size = new System.Drawing.Size(86, 23);
+            this.loadMessage.TabIndex = 10;
+            this.loadMessage.Text = "Load image";
+            this.loadMessage.UseVisualStyleBackColor = true;
+            this.loadMessage.Click += new System.EventHandler(this.loadMessage_Click);
+            // 
+            // loadCover
+            // 
+            this.loadCover.Location = new System.Drawing.Point(81, 252);
+            this.loadCover.Name = "loadCover";
+            this.loadCover.Size = new System.Drawing.Size(86, 23);
+            this.loadCover.TabIndex = 9;
+            this.loadCover.Text = "Load image";
+            this.loadCover.UseVisualStyleBackColor = true;
+            this.loadCover.Click += new System.EventHandler(this.loadCover_Click);
+            // 
+            // picMessage
+            // 
+            this.picMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picMessage.Location = new System.Drawing.Point(238, 51);
+            this.picMessage.Name = "picMessage";
+            this.picMessage.Size = new System.Drawing.Size(108, 108);
+            this.picMessage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picMessage.TabIndex = 8;
+            this.picMessage.TabStop = false;
+            this.picMessage.Click += new System.EventHandler(this.picMessage_Click);
+            // 
+            // picCover
+            // 
+            this.picCover.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picCover.Location = new System.Drawing.Point(16, 30);
+            this.picCover.Name = "picCover";
+            this.picCover.Size = new System.Drawing.Size(216, 216);
+            this.picCover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picCover.TabIndex = 7;
+            this.picCover.TabStop = false;
+            this.picCover.Click += new System.EventHandler(this.picCover_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.helpToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(585, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "View";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showHelpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem1.Text = "Help";
+            // 
+            // showHelpToolStripMenuItem
+            // 
+            this.showHelpToolStripMenuItem.Name = "showHelpToolStripMenuItem";
+            this.showHelpToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showHelpToolStripMenuItem.Text = "Show help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 285);
+            this.Controls.Add(this.picStego);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.loadMessage);
+            this.Controls.Add(this.loadCover);
+            this.Controls.Add(this.picMessage);
+            this.Controls.Add(this.picCover);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picStego)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMessage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCover)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picStego;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Button loadMessage;
+        private System.Windows.Forms.Button loadCover;
+        private System.Windows.Forms.PictureBox picMessage;
+        private System.Windows.Forms.PictureBox picCover;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showHelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+    }
+}
+
---
+++ Programmer/GUI/StegoGUI/StegoGUI/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StegoGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.AutoSize = false;
+            label1.Height = 2;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void picCover_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadMessage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadCover_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picMessage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picStego_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
