3e04dad83e130efa1ee6e59fc393fa3bec841e80
Henrik <hhs19942@gmail.com>
2016-04-29 12:14:45 +0200

    More debugging. Oh and a loop that runs 3 billion times

--- Programmer/Stegosaurus/ConsoleTester/Program.cs
+++ Programmer/Stegosaurus/ConsoleTester/Program.cs
@@ -8,11 +8,11 @@ namespace ConsoleTester {
     class Program {
         static void Main(string[] args) {
             IImageEncoder ji = new JpegImage(new Bitmap(@"cat.jpg"), 100, 4);
-            
-            //ji.Encode(new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 54, 12, 56, 12, 12, 12, 12, 45, 76, 23, 54});
+            //Console.WriteLine(ji.GetCapacity());
+            ji.Encode(new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 54, 12, 56, 12, 12, 12, 12, 45, 76, 23, 54});
             //ji.Save(@"output.jpg");
 
-            debugGraph();
+            //debugGraph();
 
             //IImageDecoder jid = new Decoder("output.jpg");
             //jid.Decode();
