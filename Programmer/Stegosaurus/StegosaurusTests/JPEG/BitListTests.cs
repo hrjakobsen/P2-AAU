fa6e88fba6cb906999795d7211bc4286ed6797b0
Antiknight <Leo@edit.fo>
2016-05-03 08:45:06 +0200

    Better names for tests

--- Programmer/Stegosaurus/StegosaurusTests/JPEG/BitListTests.cs
+++ Programmer/Stegosaurus/StegosaurusTests/JPEG/BitListTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using Stegosaurus;
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ namespace Stegosaurus.Tests
     public class BitListTests
     {
         [Test()]
-        public void BitListTest()
+        public void BitListTest_Construction_Of_Zero_Length_Bitlist()
         {
             BitList bl = new BitList();
 
@@ -20,11 +21,25 @@ namespace Stegosaurus.Tests
         }
 
         [Test()]
-        public void BitListTest1()
+        public void BitListTest_Construction_Of_One_Length_Bitlist()
         {
             BitList bl = new BitList(1);
 
             Assert.AreEqual(1, bl.Count);
+        }
+
+        [Test()]
+        public void BitListTest_Construction_Of_negative_length_Bitlist() //TODO: do checks on this test
+        {
+            try
+            {
+                BitList bl = new BitList(-1);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Assert.Pass();
+            }
+
         }
 
         [Test()]
@@ -37,7 +52,7 @@ namespace Stegosaurus.Tests
         }
 
         [Test()]
-        public void InsertTest()
+        public void InsertTest_Insertion_On_Index()
         {
             BitList bl = new BitList(8);
 
@@ -47,23 +62,23 @@ namespace Stegosaurus.Tests
         }
 
         [Test()]
-        public void AddTest() //TODO: Seems like Add() changes the last value in bitlist and does not add a bit to the bitlist. Don't know if this is intended
+        public void AddTest_If_Add_Bool_Val_Adds_bit()
         {
             BitList bl = new BitList(8);
 
             bl.Add(true);
 
-            Assert.AreEqual(true, bl[9]);
+            Assert.AreEqual(true, bl[8]);
         }
 
         [Test()]
-        public void AddTest1()
+        public void AddTest_If_Add_Int_Val_Adds_bit()
         {
             BitList bl = new BitList(8);
 
             bl.Add(1);
 
-            Assert.AreEqual(true, bl[9]);
+            Assert.AreEqual(true, bl[8]);
         }
     }
 }
