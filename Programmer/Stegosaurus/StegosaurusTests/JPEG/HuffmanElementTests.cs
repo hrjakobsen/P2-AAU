57f9fe2c7cb5790a9649377235fcb458481ff335
Leo Mohr <leo@jmohr.net>
2016-05-09 20:13:03 +0200

    Huffman Element tests done

--- Programmer/Stegosaurus/StegosaurusTests/JPEG/HuffmanElementTests.cs
+++ Programmer/Stegosaurus/StegosaurusTests/JPEG/HuffmanElementTests.cs
@@ -11,28 +11,75 @@ namespace Stegosaurus.Tests
     [TestFixture()]
     public class HuffmanElementTests
     {
-        [Test()]
-        public void HuffmanElementTest()
+        /* TODO: Find out how to test huffman elements correctly
+        public HuffmanElement huffmanTestElement1;
+        public HuffmanElement huffmanTestElement2;
+
+        [SetUp()]
+        public void Huffelements()
         {
-            Assert.Fail();
+            byte runSizeInput1 = (byte)00000000;
+            ushort codeWordInput1 = 2;
+            byte lengthInput1 = (byte)8;
+
+            HuffmanElement huffmanTestElement1 = new HuffmanElement(runSizeInput1, codeWordInput1, lengthInput1);
+
+            byte runSizeInput2 = Byte.Parse("11111111");
+            ushort codeWordInput2 = UInt16.Parse("00000111");
+            byte lengthInput2 = Byte.Parse("0000100");
+
+            HuffmanElement huffmanTestElement2 = new HuffmanElement(runSizeInput2, codeWordInput2, lengthInput2);
+        }
+        */
+
+        [Test()]
+        public void CompareToTest1()
+        {
+            byte runSizeInput1 = 00000000;
+            ushort codeWordInput1 = 2;
+            byte lengthInput1 = 8;
+
+            byte runSizeInput2 = 1;
+            ushort codeWordInput2 = 3;
+            byte lengthInput2 = 8;
+
+            HuffmanElement huffmanTestElement1 = new HuffmanElement(runSizeInput1, codeWordInput1, lengthInput1);
+
+            HuffmanElement huffmanTestElement2 = new HuffmanElement(runSizeInput2, codeWordInput2, lengthInput2);
+
+            var check = huffmanTestElement1.CompareTo(huffmanTestElement2);
+            Assert.AreEqual(-1, check);
         }
 
         [Test()]
-        public void CompareToTest()
+        public void CompareToTest2()
         {
-            Assert.Fail();
-        }
+            byte runSizeInput1 = 00000000;
+            ushort codeWordInput1 = 2;
+            byte lengthInput1 = 7;
 
-        [Test()]
-        public void CodeWordToBytesTest()
-        {
-            Assert.Fail();
+            byte runSizeInput2 = 1;
+            ushort codeWordInput2 = 3;
+            byte lengthInput2 = 8;
+
+            HuffmanElement huffmanTestElement1 = new HuffmanElement(runSizeInput1, codeWordInput1, lengthInput1);
+
+            HuffmanElement huffmanTestElement2 = new HuffmanElement(runSizeInput2, codeWordInput2, lengthInput2);
+
+            var check = huffmanTestElement1.CompareTo(huffmanTestElement2);
+            Assert.AreEqual(-1, check);
         }
 
         [Test()]
         public void ToStringTest()
         {
-            Assert.Fail();
+            byte runSizeInput1 = 00000000;
+            ushort codeWordInput1 = 2;
+            byte lengthInput1 = 8;
+
+            HuffmanElement huffmanTestElement1 = new HuffmanElement(runSizeInput1, codeWordInput1, lengthInput1);
+
+            Assert.AreEqual("0 = 10, 1000", huffmanTestElement1.ToString());
         }
     }
 }
