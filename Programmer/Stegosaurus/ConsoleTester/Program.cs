f33794f3ac3cbceec35d75e5fdef6677350a6bb8
Henrik <hhs19942@gmail.com>
2016-05-12 10:21:44 +0200

    Waay broken code

--- Programmer/Stegosaurus/ConsoleTester/Program.cs
+++ Programmer/Stegosaurus/ConsoleTester/Program.cs
@@ -7,24 +7,23 @@ using System.Linq;
 namespace ConsoleTester {
     class Program {
         static void Main(string[] args) {
-            IImageEncoder ji = new JpegImage(new Bitmap(@"cat2.jpg"), 100, 4);
+            IImageEncoder ji = new JpegImage(new Bitmap(@"testImage.jpg"), 100, 4);
             
             //Console.WriteLine(ji.GetCapacity());
 
             //byte[] msg = "Hej".Select(x => (byte)x).ToArray();
 
-            int len = 400;
+            int len = 320;
             byte[] msg = new byte[len];
             for (int i = 0; i < len; i++) {
                 msg[i] = (byte)('a' + i % 26);
             }
 
             ji.Encode(msg);
-            ji.Save(@"cat2Out.jpg");
+            ji.Save(@"testOut.jpg");
 
-            IImageDecoder jid = new Decoder("cat2Out.jpg");
+            IImageDecoder jid = new Decoder("testOut.jpg");
             byte[] message = jid.Decode();
-            Console.WriteLine(message.Length);
             Console.WriteLine(new string(message.Select(x => (char)x).ToArray()));
             Console.ReadKey();
         }
