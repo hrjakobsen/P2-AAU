d5b1c2a16188cdb4e44bb46328d921ced6418d42
Mathias Jakobsen <mathias@jbcoding.com>
2016-05-19 12:00:16 +0200

    Changes in the start of the report and to problem confinement. Also a conclusion to the testing section

--- Programmer/Stegosaurus/ConsoleTester/Program.cs
+++ Programmer/Stegosaurus/ConsoleTester/Program.cs
@@ -7,22 +7,24 @@ using System.Linq;
 namespace ConsoleTester {
     class Program {
         static void Main(string[] args) {
-            //IImageEncoder ji = new JpegImage(new Bitmap(@"tiger.jpg"), 100, 4);
-            IImageEncoder ji = new LeastSignificantBitImage(new Bitmap(@"cat.jpg"));
-            Console.WriteLine(ji.GetCapacity());
+            //IImageEncoder ji = new JpegImage(new Bitmap(@"cat.jpg"), 100, 4);
+            //IImageEncoder ji = new LeastSignificantBitImage(new Bitmap(@"cat.jpg"));
+            //Console.WriteLine(ji.GetCapacity());
 
-            int len = 280;
-            byte[] msg = new byte[len];
-            for (int i = 0; i < len; i++) {
-                msg[i] = (byte)('A' + i % 26);
-            }
+            
 
-            ji.Encode(msg);
-            ji.Save(@"out.png");
+            //int len = 280;
+            //byte[] msg = new byte[len];
+            //for (int i = 0; i < len; i++) {
+            //    msg[i] = (byte)('A' + i % 26);
+            //}
 
-            IImageDecoder jid = new LeastSignificantBitDecoder("out.png");
-            byte[] message = jid.Decode();
-            Console.WriteLine(new string(message.Select(x => (char)x).ToArray()));
+            //ji.Encode(msg);
+            //ji.Save(@"out.jpg");
+
+            //IImageDecoder jid = new JPEGDecoder("out.jpg");
+            //byte[] message = jid.Decode();
+            //Console.WriteLine(new string(message.Select(x => (char)x).ToArray()));
             Console.ReadKey();
         }
     }
