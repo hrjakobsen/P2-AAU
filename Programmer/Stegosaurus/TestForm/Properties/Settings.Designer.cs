2e71d9537ee969ed5d42963680a6db3ddff97c3c
Mathias Jakobsen <mathias@jbcoding.com>
2016-05-23 14:35:22 +0200

    Fuuuck

--- Programmer/Stegosaurus/TestForm/Properties/Settings.Designer.cs
+++ Programmer/Stegosaurus/TestForm/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TestForm.Properties {
+namespace SteGUI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
