eae1ce8e0dc623d98a31c0e8be4e862618827e31
Flax95 <95soren@gmail.com>
2016-05-23 14:30:29 +0200

    GUI, everything that has to do with settings is handled in OptionsForm

--- Programmer/Stegosaurus/TestForm/Properties/Settings.Designer.cs
+++ Programmer/Stegosaurus/TestForm/Properties/Settings.Designer.cs
@@ -80,7 +80,7 @@ namespace TestForm.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("53")]
+        [global::System.Configuration.DefaultSettingValueAttribute("80")]
         public int Quality {
             get {
                 return ((int)(this["Quality"]));
